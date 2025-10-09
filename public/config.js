73d6b4cc4f2563d38215c6f1eb53477e3a2661e1
Mario Reder <mario.reder@pm.me>
2025-10-09 08:32:42 +0200

    Update DEX configuration and branding

--- public/config.js
+++ public/config.js
@@ -25,7 +25,7 @@ window.__RUNTIME_CONFIG__ = {
   "VITE_TWITTER_URL": "",
   "VITE_SEO_SITE_NAME": "Perpsx.com",
   "VITE_SEO_SITE_DESCRIPTION": "",
-  "VITE_SEO_SITE_URL": "https://perpsx.com",
+  "VITE_SEO_SITE_URL": "https://dexgro.com",
   "VITE_SEO_SITE_LANGUAGE": "en",
   "VITE_SEO_SITE_LOCALE": "en_US",
   "VITE_SEO_TWITTER_HANDLE": "",
